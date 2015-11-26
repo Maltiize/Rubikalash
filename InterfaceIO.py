d7512e4939d16939fc0bd5be572fe9f9ae6b488c
Thomas Coquereau <thomas.coquereau@etu.univ-nantes.fr>
2015-11-26 17:07:08 +0100

    Squelette interface IO

---
+++ InterfaceIO.py
@@ -0,0 +1,14 @@
+class InterfaceIO:
+
+#classe qui gère les entrées sorties avec l'utilisateur
+    def __init__(self, entree=None, sortie=None):
+        self.entree=entree
+        self.sortie=sortie
+
+#récupère le cube de départ
+    def PrendEntree(self):
+
+#renvoie les mouvements a faire a l'utilisateur
+    def EnvoieSortie(self):
+        
+        
