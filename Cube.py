bba8c8f786f67d72c87f8758c7872b3e295d46b6
Etienne MARTIN <etienne.martin2@etu.univ-nantes.fr>
2015-11-26 19:18:43 +0100

    fix __init__ de Cube + methode d'affichage basique du cube

--- Cube.py
+++ Cube.py
@@ -7,22 +7,22 @@ class Cube:
         #plages d'indexes de toutes les cases correspondantes à chaque face
         # CF schéma dans madoc 
 
-        self.idxu=range(0,7)
-        self.idxd=range(40,47)
-        self.idxf=[11,12,13,22,23,31,32,33]
-        self.idxl=[8,9,10,20,21,28,29,30]
-        self.idxr=[14,15,16,24,25,34,35,36]
-        self.idxb=[17,18,19,26,27,37,38,39]
+        self.idxu=range(0,9)
+        self.idxd=range(45,54)
+        self.idxf=[12,13,14 ,24,25,26, 36,37,3]
+        self.idxl=[9,10,11, 21,22,23, 33,34,35]
+        self.idxr=[15,16,17 ,27,28,29, 39,40,41]
+        self.idxb=[18,19,20 ,30,31,32, 42,43,44]
 
         
         #Initialisation des faces à 0
         
-        self.up=[0,0,0],[0,"U",0],[0,0,0] 
-        self.down=[0,0,0],[0,"D",0],[0,0,0]
-        self.front=[0,0,0],[0,"F",0],[0,0,0]
-        self.left=[0,0,0],[0,"L",0],[0,0,0]
-        self.right=[0,0,0],[0,"R",0],[0,0,0]
-        self.back=[0,0,0],[0,"B",0],[0,0,0]
+        self.up=[0,0,0],[0,0,0],[0,0,0] 
+        self.down=[0,0,0],[0,0,0],[0,0,0]
+        self.front=[0,0,0],[0,0,0],[0,0,0]
+        self.left=[0,0,0],[0,0,0],[0,0,0]
+        self.right=[0,0,0],[0,0,0],[0,0,0]
+        self.back=[0,0,0],[0,0,0],[0,0,0]
 
 
         #Si aucunes configuration de départ n'a été renseignée
@@ -33,48 +33,33 @@ class Cube:
         self.origin=cube
 
         
-        up=setface(idxu,up)
-        down=setface(idxd,down)
-        front=setface(idxf,front)
-        left=setface(idxl,left)
-        right=setface(idxr,right)
-        back=setface(idxb,back)
+        self.up=self.setface(self.idxu,self.up)
+        self.down=self.setface(self.idxd,self.down)
+        self.front=self.setface(self.idxf,self.front)
+        self.left=self.setface(self.idxl,self.left)
+        self.right=self.setface(self.idxr,self.right)
+        self.back=self.setface(self.idxb,self.back)
 
 
     # Cette methode remplis chaque face avec les éléments qui lui correspondent 
     # ( renseignés dans les listes idx[nomface] )
 
     def setface(self,idx,face):
-        if(len(idx)!=8):
+        if(len(idx)!=9):
             print("INVALID SIZE ON IDX")
             return -1
         
         i=0
         j=0
         for val in idx:
-            
-            # cette condition sert à "sauter" la case avec le nom de la face
-            # R L F B U ou D
-            
-            if(face[j][i]==0): 
-                face[j][i]=self.origin[val]
-            else:
-                
-                # si on tombe sur la case avec le nom de la face
-                # on remplis la case juste apres
-                # et on saute une ligne (j+1)
-                
-                face[j][i+1]=self.origin[val]
-                j=j+1
-                
+
+            face[j][i]=self.origin[val]
             #on passe à la case suivante   
             i=i+1
-            
-            if(i%3==0):
-                
-                # si on arrive à la fin d'une ligne
-                # on passe à la suivante
-                
+
+            # si on arrive à la fin d'une ligne
+            # on passe à la suivante         
+            if(i%3==0):   
                 i=0
                 j=j+1
 
@@ -117,3 +102,41 @@ class Cube:
     def cubeFinished(self):
         return
 
+
+    #methode d'affichage du cube
+    def printcube(self):
+        afftab(self.up)
+        afftab(self.left)
+        afftab(self.front)
+        afftab(self.right)
+        afftab(self.back)
+        afftab(self.down)
+
+#methode d'affichage d'un table 2D
+def afftab(tab):
+    for x in tab:
+        aff(x)
+    print("")
+    print("")
+
+        
+
+
+       
+def aff(tab):
+    for x in tab:
+        print (x,end='')
+        print(" , ",end='')
+    print("")
+
+
+
+        
+
+
+cube = Cube("OGRBWYBGBGYYOYOWOWGRYOOOBGBRRYRBWWWRBWYGROWGRYBRGYWBOG")
+
+
+    
+
+cube.printcube()
