d6442fc509ce82cd2b35782a3f8131e570274c45
Thomas Coquereau <thomas.coquereau@etu.univ-nantes.fr>
2015-12-22 17:25:59 +0100

    Ajout recherche face suivant couleur

--- Resolution.py
+++ Resolution.py
@@ -4,23 +4,40 @@
 from Cube import Cube
 class Resolution:
 
-    def __init__(self,c):
+    def __init__(self,c,face=None):
         self.cube=c
+        self.face=face
 
-
-
-
-    def retournerDeuxAretes(self,nameFace):
-        print("cube.rotation")
+#Fonction qui renvoie quelle face est de la couleur recherchée (jaune pour mon algo ("Y"))
+    def whichIsColor(self,color):
+        
+        colorF = cube.up[1][1]
+        if colorF == 'Y' :
+            return "u"
+        colorF = cube.down[1][1]
+        if colorF == 'Y' :
+            return "d"
+        colorF = cube.right[1][1]
+        if colorF == 'Y' :
+            return "r"
+        colorF = cube.left[1][1]
+        if colorF == 'Y' :
+            return "l"
+        colorF = cube.back[1][1]
+        if colorF == 'Y' :
+            return "b"
+        colorF = cube.front[1][1]
+        if colorF == 'Y':
+            return "f"
         
         
         
 
 
-cube = Cube("GGGGGGGGGOOOOOOOOOYYYYYYYYYRRRRRRRRRBBBBBBBBBWWWWWWWWW")
+cube = Cube("GGGGGGOGROOBYYGYRRWWWOORYYYBRRWWWOORBYYORRWWWYOGBBBBBB")
 
 resolution = Resolution(cube)
 
 cube.printCube()
         
-        
+print(resolution.whichIsColor('Y'))
