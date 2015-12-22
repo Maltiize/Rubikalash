10e2f783dce13421ff3d79f427d4c1052554381f
Thomas Coquereau <thomas.coquereau@etu.univ-nantes.fr>
2015-12-22 17:34:40 +0100

    Ajout recherche des aretes manquantes

--- Resolution.py
+++ Resolution.py
@@ -4,9 +4,10 @@
 from Cube import Cube
 class Resolution:
 
-    def __init__(self,c,face=None):
+    def __init__(self,c,face=None,listeAretes=[]):
         self.cube=c
         self.face=face
+        self.listeAretes=listeAretes
 
 #Fonction qui renvoie quelle face est de la couleur recherchée (jaune pour mon algo ("Y"))
     def whichIsColor(self,color):
@@ -29,7 +30,13 @@ class Resolution:
         colorF = cube.front[1][1]
         if colorF == 'Y':
             return "f"
-        
+
+#Fonction qui recherche les aretes manquantes puis effectue les rotations nécessaires pour les amener
+    def solveYellow(self):
+        listeColors=['G','R','B','O']
+        for i in range(len(listeColors)):   
+            self.listeAretes.append(cube.findCube(['Y',listeColors[i]]))
+        #trouve toutes les aretes jaunes que l'on doit placer         
         
         
 
@@ -40,4 +47,4 @@ resolution = Resolution(cube)
 
 cube.printCube()
         
-print(resolution.whichIsColor('Y'))
+resolution.solveYellow()
